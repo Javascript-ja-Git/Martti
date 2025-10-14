9832f3f8c0f12e5a6b3153627fbf680b44592a4b
Martti Raavel <mrt@mrt.ee>
2025-10-14 17:13:49 +0300

    Esimeses loengus kirjutatud kood

---
+++ code/01/app.js
@@ -0,0 +1,40 @@
+// alert('Tere tulemast!');
+
+// confirm("Kas tahaksid lõunale minna?");
+
+// let firstName = 'Martti';
+
+// firstName = 'Miki';
+
+// alert(firstName);
+
+// firstName = prompt("Palun sisesta oma eesnimi");
+
+// alert(firstName);
+
+// let choice;
+
+// choice = confirm('Vali ei või jah');
+
+// alert(choice);
+
+// const number1 = prompt('Sisesta esimene number');
+// const number2 = prompt('Sisesta teine number');
+
+// alert(number1 - number2);
+
+// let age = 30;
+// alert(age);
+// age += 1;
+// age = age + 1;
+// alert(age);
+
+// const password = prompt('Sisesta parool');
+
+// alert(password == 123);
+
+a = 1;
+b = 2;
+c = a + b;
+a = 10;
+console.log(c);
