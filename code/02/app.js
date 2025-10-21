c90b3b6f4b01f672cf5e3ef209452e3214184e38
Martti Raavel <mrt@mrt.ee>
2025-10-21 13:03:59 +0300

    Harjutused muutujatega

--- code/02/app.js
+++ code/02/app.js
@@ -1,2 +1,38 @@
+// Tervituse programm
+/*
 const name = prompt('Mis Su nimi on?');
 alert('Tere ' + name);
+*/
+
+// Vanuse arvutamine
+
+// alert('Sinu vanus on: ' + (2025 - prompt('Sisesta oma vanus')))
+
+/* const year = prompt('Sisesta oma sünniaasta');
+const currentYear = 2025;
+const age = currentYear - year;
+
+alert('Sinu vanus on: ' + age); */
+
+// Risküliku pindala arvutamine
+
+/* const width = prompt('Sisesta ristküliku laius');
+const height = prompt('Sisesta ristküliku kõrgus');
+
+const area = width * height;
+alert('Ristküliku pindala on: ' + area); */
+
+/* // Temperatuuride teisendaja
+
+// Esimene versioon
+const temperatureCelsius = prompt('Palun sisesta temperatuur Celsiuse kraadides');
+const temperatureFarenheit = temperatureCelsius * 9 / 5 + 32;
+
+alert('Temperatuur Farnheiti skaalal: ' + temperatureFarenheit);
+
+// Teine versioon
+const C = prompt('Palun sisesta temperatuur Celsiuse kraadides');
+const F = C * 9 / 5 + 32;
+
+alert('Temperatuur Farnheiti skaalal: ' + F);
+*/
